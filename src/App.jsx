29a1db817e6c4b0d76ecba745061c26fc24f36ec
Oxypetalum7 <yuu.dtm.7@gmail.com>
2021-08-21 21:15:40 +0900

    fix style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import fukidashi from './fukidashi.png'
+import {AppBar, Typography, Toolbar, Box, Button, createTheme, ThemeProvider, Container, Grid, TextField} from '@material-ui/core'
 import './App.css'
+import { blue } from '@material-ui/core/colors';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Noto Sans JP"
+  },
+  palette: {
+    primary: blue,
+  },
+  })
 function App() {
   const [text, setName] = useState("");
   const  [context,setContext] = useState(null)
@@ -39,30 +50,48 @@ function App() {
 
   return (
     <div className="App">
-
-      <header className="App-header" style={{textAlign:'center'}}>
-        <h1>
-          プラカードジェネレータ
-        </h1>
-      </header>
-        <body className="body">
-          <p>
-          プラカードに入れたいテキストを下に入力して、"Generate"ボタンを押してください。
-          </p>
-          <input type="text" name="name" value={text}onChange={(e) => { setName(e.target.value) }}/>
-          <button onClick={DrawImage} id="generatebtn" >
-            Generate
-          </button>
-          <br/>
-          <br/>
-          <div className="canvasWrapper">
-            <canvas width="1280" height="720" id="canvas"></canvas>
-          </div>
-          <br/>
-          <button id="download" onClick={DownloadImage}>
-            Download
-          </button>
-        </body>
+      <ThemeProvider theme={theme}> 
+      <AppBar>
+          <Toolbar display="flex" justifyContent="center" alignItems="center" >
+                <Box>
+                    <Typography variant="h5">
+                        プラカードジェネレータ
+                    </Typography>
+                </Box>
+          </Toolbar>
+        </AppBar>
+        <Container className='body'>
+          <Grid container justifyContent='center' spacing={5}>
+            <Grid item>
+              <Typography variant='body'>
+              プラカードに入れたいテキストを下に入力して、"Generate"ボタンを押してください。
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container justifyContent='center' spacing={5}>
+            <Grid item>
+                <TextField id="outlined-basic" label="Text" variant="outlined" value={text} onChange={(e) => { setName(e.target.value) }}/>
+            </Grid>
+            <Grid item>
+              <Button onClick={DrawImage} id="generatebtn" color="primary">
+                Generate
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container justifyContent='center' spacing={5}>
+            <Grid item>
+              <Box className="canvasWrapper">
+                <canvas width="1280" height="720" id="canvas"></canvas>
+              </Box>
+            </Grid>
+          </Grid>
+          <Grid container justifyContent='center' spacing={3}>
+            <Button id="download" label='Download' onClick={DownloadImage} color="primary">
+              Download
+            </Button>
+          </Grid>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
